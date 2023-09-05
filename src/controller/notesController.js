355722ac089bb07f5dee287cec2130ec2b3f70ee
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-09-05 15:26:29 -0300

    function for create the note created

--- src/controller/notesController.js
+++ src/controller/notesController.js
@@ -1,8 +1,29 @@
+const knex = require("../database/knex");
 class NotesController {
   async create(request, response) {
+    const { title, description, rating, tags } = request.body;
     const { user_id } = request.params;
 
-    return response.json(user_id);
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      rating,
+      user_id,
+    });
+
+    const tagsInsert = tags.map((name) => {
+      return {
+        note_id,
+        user_id,
+        name,
+      };
+    });
+
+    await knex("tags").insert(tagsInsert);
+
+    return response.json({
+      message: "note created",
+    });
   }
 }
 
