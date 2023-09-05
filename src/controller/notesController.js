d083852d613fb4f8f8d13295cce7b6cc48cd7435
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-09-05 16:10:13 -0300

    function search notes and title and tags created

--- src/controller/notesController.js
+++ src/controller/notesController.js
@@ -51,6 +51,42 @@ class NotesController {
       message: "note deleted",
     });
   }
+
+  async index(request, response) {
+    const { user_id, title, tags } = request.query;
+
+    let notes;
+
+    if (tags) {
+      const filterTags = tags.split(",").map((tag) => tag.trim());
+
+      notes = await knex("tags")
+        .select(["notes.id", "notes.title", "notes.user_id"])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title");
+    } else {
+      notes = await knex("notes")
+        .whereLike("title", `%${title}%`)
+        .where({ user_id })
+        .orderBy("title");
+    }
+
+    const allTags = await knex("tags").where({ user_id });
+
+    const notesWithTags = notes.map((note) => {
+      const filterTags = allTags.filter((tag) => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: filterTags,
+      };
+    });
+
+    return response.json(notesWithTags);
+  }
 }
 
 module.exports = NotesController;
