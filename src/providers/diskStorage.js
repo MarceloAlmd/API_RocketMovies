cd9f5d0571e3c6f548b554f7f53a963294882d9d
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-10-10 14:40:29 -0300

    feat: class provider for save file and delete file

---
+++ src/providers/diskStorage.js
@@ -0,0 +1,28 @@
+const path = require("node:path");
+const fs = require("node:fs");
+const uploadsConfig = require("../configs/uploads");
+
+class DiskStorage {
+  async saveFile(filename) {
+    await fs.promises.rename(
+      path.resolve(uploadsConfig.TMP_FOLDER, filename),
+      path.resolve(uploadsConfig.UPLOADS_FOLDER, filename)
+    );
+
+    return filename;
+  }
+
+  async deleteFile(filename) {
+    const filePath = path.resolve(uploadsConfig.UPLOADS_FOLDER, filename);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
+
+module.exports = DiskStorage;
