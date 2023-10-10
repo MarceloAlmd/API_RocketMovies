bed28601ec26516e81e9f32ffb09d53767e3c108
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-10-10 14:39:46 -0300

    feat: create function for uploads avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,23 @@
 const { Router } = require("express");
 const usersRoutes = Router();
-
+const multer = require("multer");
+const uploadsConfig = require("../configs/uploads");
 const UsersController = require("../controller/usersController");
+const UserAvatarController = require("../controller/userAvatarController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
+const upload = multer(uploadsConfig.MULTER);
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
 usersRoutes.delete("/", ensureAuthenticated, usersController.delete);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = usersRoutes;
