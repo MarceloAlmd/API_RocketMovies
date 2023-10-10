ea5b291db9bec0078e336b5fc339efc2fe899a54
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-10-10 14:44:16 -0300

    feat: show avatar img

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 require("express-async-errors");
 
 const express = require("express");
+const uploadsConfig = require("./configs/uploads");
 const routes = require("./routes");
 const serverError = require("./utils/serverError");
 const sqliteConnection = require("./database/sqlite");
 const app = express();
 sqliteConnection();
 app.use(express.json());
-
+app.use("/files", express.static(uploadsConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 app.use((error, request, response, next) => {
